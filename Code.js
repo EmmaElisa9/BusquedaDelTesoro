2bdb13302af488d6416a92e368f8fba2b7965f53
EmmaElisa9 <89869116+EmmaElisa9@users.noreply.github.com>
2022-01-07 12:21:45 -0600

    Add files via upload

---
+++ Code.js
@@ -0,0 +1,20 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white");
+    textSize(15);
+    text("R E V B A I L A",10,50);
+
+    text("C U N I F O N",300,50);
+
+    text("E D A T E D O S B A S",135,50);
+
+    fill("lightblue");
+    text("Pista: Siempre cambiando, no constante!!",100,70);
+    text("responde las tres casillas segun la pista de arriba intentando acomodar las letras como deben ir",100,90);
+    // add code for displaying rest of the names and the hints.
+
+}
